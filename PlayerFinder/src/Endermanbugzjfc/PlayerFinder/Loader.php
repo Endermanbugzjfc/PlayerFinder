7ca8686dcd344ae521cb31082291b5ffd4d0f884
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-29 02:36:35 +0800

    Added loader

---
+++ PlayerFinder/src/Endermanbugzjfc/PlayerFinder/Loader.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\PlayerFinder;
+
+use pocketmine\plugin\PluginBase;
+
+final class Loader extends PluginBase
+{
+    protected function onEnable() : void
+    {
+    }
+
+    protected function onDisable() : void
+    {
+    }
+}
