7a6cb6ff9b69a2885d104e121fc21f026f020435
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-29 03:00:11 +0800

    Added libasynql stuff

--- PlayerFinder/src/Endermanbugzjfc/PlayerFinder/Plugin/MainClass.php
+++ PlayerFinder/src/Endermanbugzjfc/PlayerFinder/Plugin/MainClass.php
@@ -5,14 +5,42 @@ declare(strict_types=1);
 namespace Endermanbugzjfc\PlayerFinder\Plugin;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use poggit\libasynql\DataConnector;
+use poggit\libasynql\libasynql;
 
 final class MainClass extends PluginBase
 {
+    private DataConnector $dataConnector;
+
     protected function onEnable() : void
     {
+        $this->loadDatabase();
+    }
+
+    private function loadDatabase() : void
+    {
+        $log = $this->getLogger();
+        $databaseConfigFile = "database.yml";
+        $databaseConfigPath = $this->getDataFolder() . $databaseConfigFile;
+        $statementMaps = [
+            "mysql" => "sql/mysql.sql",
+            "sqlite" => "sql/sqlite.sql"
+        ];
+
+        $log->info("Establishing connection to database (to track offline player names and store player settings)...");
+        $this->saveResource($databaseConfigFile);
+        $databaseConfig = (new Config($databaseConfigPath))->getAll();
+        $this->dataConnector = libasynql::create($this, $databaseConfig, $statementMaps);
+        $log->info("Completed.");
     }
 
     protected function onDisable() : void
     {
+        $connector = $this->dataConnector ?? null;
+        if ($connector !== null) {
+            $connector->close();
+            unset($this->dataConnector);
+        }
     }
 }
