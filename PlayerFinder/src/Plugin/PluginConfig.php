1e8c5fbe26c4e35b20242a711e2d4a892166d542
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-29 03:50:05 +0800

    Added command profiles in config

--- PlayerFinder/src/Plugin/PluginConfig.php
+++ PlayerFinder/src/Plugin/PluginConfig.php
@@ -7,4 +7,28 @@ namespace Endermanbugzjfc\PlayerFinder\Plugin;
 final class PluginConfig
 {
     public bool $samePageForSearchBarAndSelector = false;
+
+    public bool $enableFindPlayerCommand = true;
+
+    public string $commandName = "findplayer";
+    public string $commandDescription = "Find a player on the server.";
+    /**
+     * @var string[]
+     */
+    public array $commandAliases = [
+        "findplayers",
+        "playerfinder",
+        "playersfinder"
+    ];
+
+    public string $commandNameExcludeOffline = "exclude-offline";
+    public string $commandDescriptionExcludeOffline = "Find an online player on the server.";
+    /**
+     * @var string[]
+     */
+    public array $commandAliasesExcludeOffline = [
+        "no-offline",
+        "online-only",
+        "only-online"
+    ];
 }
