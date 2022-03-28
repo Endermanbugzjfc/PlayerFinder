f9a49274c306f8aa9226e6feaa73fffd19b09902
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-29 02:47:15 +0800

    Added parameter data class

---
+++ PlayerFinder/src/Endermanbugzjfc/PlayerFinder/Parameter.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+final class Parameter
+{
+    public bool $showOfflinePlayers = true;
+
+    public int $minTargets = 0;
+    public int $maxTargets = 0;
+
+    public string $searchBarDefaultValue = "";
+}
