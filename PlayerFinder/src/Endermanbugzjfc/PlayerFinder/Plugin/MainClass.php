e4efe417f16f9eba40402d555fe1ab2a65636462
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-29 03:19:51 +0800

    Load plugin config

--- PlayerFinder/src/Endermanbugzjfc/PlayerFinder/Plugin/MainClass.php
+++ PlayerFinder/src/Endermanbugzjfc/PlayerFinder/Plugin/MainClass.php
@@ -4,18 +4,39 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\PlayerFinder\Plugin;
 
+use Endermanbugzjfc\ConfigStruct\Emit;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use poggit\libasynql\DataConnector;
 use poggit\libasynql\libasynql;
+use function file_exists;
+use function file_put_contents;
 
 final class MainClass extends PluginBase
 {
     private DataConnector $dataConnector;
 
+    private PluginConfig $configObject;
+
     protected function onEnable() : void
     {
+        $this->loadConfig();
         $this->loadDatabase();
+    }
+
+    private function loadConfig() : void
+    {
+        $path = $this->getDataFolder() . "config.yml";
+        $object = new PluginConfig();
+        if (file_exists($path)) {
+            $data = (new Config($path))->getAll();
+            $context = Parse::object($object, $data);
+            $context->copyToObject($object, $path);
+        } else {
+            $data = Emit::object($object);
+            file_put_contents($path, $data);
+        }
+        $this->configObject = $object;
     }
 
     private function loadDatabase() : void
@@ -49,5 +70,6 @@ final class MainClass extends PluginBase
             $connector->close();
             unset($this->dataConnector);
         }
+        unset($this->configObject);
     }
 }
